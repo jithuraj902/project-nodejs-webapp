954382dab80c303d94dd2401c081a1f2e9ccabf3
Alok Agrawal <alokagarwal111@gmail.com>
2017-11-10 18:03:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,13 @@
+'use strict';
 var http = require('http');
-var port = process.env.port || 1337;
-var fs = require("fs");
-var url = require("url");
-var path = require("path");
+var port = process.env.PORT || 8092;
+var fs = require('fs');
 
-http.createServer(function (request, response) {
-    var match = request.url.match(/^\/img\/.+/);
-    if (match !== null) {
-        var len = request.url.length;
-        var filename = request.url.substr(1, len);        
-        sendFileContent(response, filename, "image/svg+xml");
+http.createServer(function (req, res) {
 
-    }
-    else
-    {
-        if (request.url === "/" || request.url === "/index") {            
-            console.log(request.url);
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            sendFileContent(response, "index.html", "text/html");            
-        }
-        else if (/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())) {
-            sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-        }
-        else if (/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())) {
-            sendFileContent(response, request.url.toString().substring(1), "text/css");
-        }
-        else if (/^\/[a-zA-Z0-9\/]*.ttf$/.test(request.url.toString())) {
-            sendFileContent(response, request.url.toString().substring(1), "font/opentype");
-        }
-        else {
-            console.log("Requested URL is: " + request.url);
-            response.end();
-        }
-    }
-}).listen(port);
-
-function sendFileContent(response, fileName, contentType) {
-    fs.readFile(fileName, function (err, data) {
-        if (err) {
-            response.writeHead(404);
-            response.write("Not Found!");
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.write(data);
-        }
-        response.end();
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        res.write(data);
+        res.end();
     });
-}
+}).listen(port);
